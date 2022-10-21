6c8620e3f89bbfe46d68b50ad78215551829517d
hanyvv <programming.han@gmail.com>
2022-10-21 23:22:04 +0900

    feat : rabbitmq 추가

--- src/main/java/com/iobound/PostController.java
+++ src/main/java/com/iobound/PostController.java
@@ -1,5 +1,7 @@
 package com.iobound;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,10 +18,14 @@ public class PostController {
     private static Integer PAGE_SIZE = 20;
 
     private final PostRepository postRepository;
+    private final Producer producer;
+    private final ObjectMapper objectMapper;
 
     @PostMapping("/post")
-    public Post createPost(@RequestBody Post post) {
-        return postRepository.save(post);
+    public Post createPost(@RequestBody Post post) throws JsonProcessingException {
+        String jsonPost = objectMapper.writeValueAsString(post);
+        producer.sendTo(jsonPost);
+        return post;
     }
 
     @GetMapping("/posts")
---
+++ src/main/java/com/iobound/Producer.java
@@ -0,0 +1,20 @@
+package com.iobound;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Producer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendTo(String message) {
+        this.rabbitTemplate.convertAndSend("CREATE_POST_QUEUE", message);
+
+    }
+
+
+
+}
