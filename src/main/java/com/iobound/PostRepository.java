924df092c20baea6250c0ee683cceb46a85d8f6b
hanyvv <programming.han@gmail.com>
2022-10-22 22:46:35 +0900

    fix : 어노테이션

--- src/main/java/com/iobound/PostRepository.java
+++ src/main/java/com/iobound/PostRepository.java
@@ -2,9 +2,11 @@ package com.iobound;
 
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends ElasticsearchRepository<Post, String> {
 
     List<Post> findByContent(String content);
